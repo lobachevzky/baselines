6137964122f25ab5b259315ae263800702a8c4ff
Ethan Brooks <ethanabrooks@gmail.com>
2017-12-24 22:03:59 -0500

    tookout wrap_deepmind

--- baselines/ppo2/run_atari.py
+++ baselines/ppo2/run_atari.py
@@ -29,11 +29,10 @@ def train(env_id, num_timesteps, seed, policy):
 
     def make_env(rank):
         def env_fn():
-            # env = make_atari(env_id)
-            env = gym.make(env_id)
+            env = make_atari(env_id)
             env.seed(seed + rank)
             env = bench.Monitor(env, logger.get_dir() and osp.join(logger.get_dir(), str(rank)))
-            return wrap_deepmind(env)
+            return env # wrap_deepmind(env)
 
         return env_fn
 
