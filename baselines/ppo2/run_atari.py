0c943fef024f47307e4c6bb2c0d1059027598a5f
Ethan Brooks <ethanabrooks@gmail.com>
2017-12-24 21:32:28 -0500

    added mlppolicy to run_atari

--- baselines/ppo2/run_atari.py
+++ baselines/ppo2/run_atari.py
@@ -3,6 +3,7 @@ import argparse
 import sys
 
 from baselines import bench, logger
+from baselines.ppo2.policies import MlpPolicy
 
 
 def train(env_id, num_timesteps, seed, policy):
@@ -18,7 +19,8 @@ def train(env_id, num_timesteps, seed, policy):
     import os.path as osp
     import tensorflow as tf
     ncpu = multiprocessing.cpu_count()
-    if sys.platform == 'darwin': ncpu //= 2
+    if sys.platform == 'darwin':
+        ncpu //= 2
     config = tf.ConfigProto(allow_soft_placement=True,
                             intra_op_parallelism_threads=ncpu,
                             inter_op_parallelism_threads=ncpu)
@@ -39,7 +41,7 @@ def train(env_id, num_timesteps, seed, policy):
     env = SubprocVecEnv([make_env(i) for i in range(nenvs)])
     set_global_seeds(seed)
     env = VecFrameStack(env, 4)
-    policy = {'cnn': CnnPolicy, 'lstm': LstmPolicy, 'lnlstm': LnLstmPolicy}[policy]
+    policy = {'cnn': CnnPolicy, 'lstm': LstmPolicy, 'lnlstm': LnLstmPolicy, 'mlp': MlpPolicy}[policy]
     ppo2.learn(policy=policy, env=env, nsteps=128, nminibatches=4,
                lam=0.95, gamma=0.99, noptepochs=4, log_interval=1,
                ent_coef=.01,
@@ -57,7 +59,7 @@ def main():
     parser.add_argument('--log-dir', help='logdir for tensorboard', default=None)
     parser.add_argument('--output-format',
                         help='comma separated list of stdout|log|json|csv|tensorboard',
-                        default='tensorboard')
+                        default='tensorboard,stdout')
     args = parser.parse_args()
     logger.configure(dir=args.log_dir, format_strs=args.output_format.split(','))
     train(args.env, num_timesteps=args.num_timesteps, seed=args.seed,
