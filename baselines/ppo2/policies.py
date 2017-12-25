23f0366c07225e577966984fdb1ab10c04d8e007
Ethan Brooks <ethanabrooks@gmail.com>
2017-12-25 17:54:52 -0500

    made `LstmPolicy` that is really just CnnPolicy in disguise

--- baselines/ppo2/policies.py
+++ baselines/ppo2/policies.py
@@ -66,18 +66,66 @@ class LnLstmPolicy(MemoryPolicy):
         return lnlstm(xs, ms, S, 'lnlstm1', nh=nh)
 
 
-class LstmPolicy(MemoryPolicy):
-    @staticmethod
-    def memory_fn(xs, ms, S, nh):
-        return lstm(xs, ms, S, 'lstm1', nh=nh)
+# class LstmPolicy(MemoryPolicy):
+#     @staticmethod
+#     def memory_fn(xs, ms, S, nh):
+#         return lstm(xs, ms, S, 'lstm1', nh=nh)
 
-    # @staticmethod
-    # def preprocess(X):
-    #     return tf.cast(X, tf.float32)
+        # @staticmethod
+        # def preprocess(X):
+        #     return tf.cast(X, tf.float32)
 
 
 class CapsulesPolicy(MemoryPolicy):
     pass
+
+
+class LstmPolicy(object):
+    def __init__(self, sess, ob_space, ac_space, nbatch, nsteps, size_mem=256, reuse=False):  # pylint: disable=W0613
+        nenv = nbatch // nsteps
+
+        nh, nw, nc = ob_space.shape
+        ob_shape = (nbatch, nh, nw, nc)
+        nact = ac_space.n
+        X = tf.placeholder(tf.uint8, ob_shape)  # obs
+
+        M = tf.placeholder(tf.float32, [nbatch])  # mask (done t-1)
+        S = tf.placeholder(tf.float32, [nenv, size_mem * 2])  # states
+
+        snew = tf.identity(S)
+
+        with tf.variable_scope("model", reuse=reuse):
+            h = conv(tf.cast(X, tf.float32) / 255., 'c1', nf=32, rf=8, stride=4, init_scale=np.sqrt(2))
+            h2 = conv(h, 'c2', nf=64, rf=4, stride=2, init_scale=np.sqrt(2))
+            h3 = conv(h2, 'c3', nf=64, rf=3, stride=1, init_scale=np.sqrt(2))
+            h3 = conv_to_fc(h3)
+            h4 = fc(h3, 'fc1', nh=512, init_scale=np.sqrt(2))
+            pi = fc(h4, 'pi', nact, act=lambda x: x, init_scale=0.01)
+            vf = fc(h4, 'v', 1, act=lambda x: x)[:, 0]
+
+        self.pdtype = make_pdtype(ac_space)
+        self.pd = self.pdtype.pdfromflat(pi)
+
+        a0 = self.pd.sample()
+        neglogp0 = self.pd.neglogp(a0)
+        self.initial_state = None
+
+
+        def step(ob, state, mask):
+            return sess.run([a0, vf, snew, neglogp0], {X: ob, S: state, M: mask})
+
+        # def step(ob, *_args, **_kwargs):
+        #     a, v, neglogp = sess.run([a0, vf, neglogp0], {X: ob})
+        #     return a, v, self.initial_state, neglogp
+
+        def value(ob, *_args, **_kwargs):
+            return sess.run(vf, {X: ob})
+
+        self.X = X
+        self.pi = pi
+        self.vf = vf
+        self.step = step
+        self.value = value
 
 
 class CnnPolicy(object):
