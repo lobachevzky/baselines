e552990488a81348de52251794eb7e5ca19bdf39
Ethan Brooks <ethanabrooks@gmail.com>
2017-12-27 19:19:09 -0500

    almost implemented routing

--- baselines/ppo2/policies.py
+++ baselines/ppo2/policies.py
@@ -65,19 +65,179 @@ class LnLstmPolicy(MemoryPolicy):
     def memory_fn(xs, ms, S, nh):
         return lnlstm(xs, ms, S, 'lnlstm1', nh=nh)
 
+    # class LstmPolicy(MemoryPolicy):
+    #     @staticmethod
+    #     def memory_fn(xs, ms, S, nh):
+    #         return lstm(xs, ms, S, 'lstm1', nh=nh)
 
-# class LstmPolicy(MemoryPolicy):
-#     @staticmethod
-#     def memory_fn(xs, ms, S, nh):
-#         return lstm(xs, ms, S, 'lstm1', nh=nh)
-
-        # @staticmethod
-        # def preprocess(X):
-        #     return tf.cast(X, tf.float32)
+    # @staticmethod
+    # def preprocess(X):
+    #     return tf.cast(X, tf.float32)
 
 
-class CapsulesPolicy(MemoryPolicy):
-    pass
+def squash(vector, epsilon=1e-9):
+    """Squashing function corresponding to Eq. 1
+    Args:
+        vector: A tensor with shape [batch_size, 1, num_caps, vec_len, 1] or [batch_size, num_caps, vec_len, 1].
+    Returns:
+        A tensor with the same shape as vector but squashed in 'vec_len' dimension.
+    """
+    vec_squared_norm = tf.reduce_sum(tf.square(vector), -2, keep_dims=True)
+    scalar_factor = vec_squared_norm / (1 + vec_squared_norm) / tf.sqrt(vec_squared_norm + epsilon)
+    vec_squashed = scalar_factor * vector  # element-wise
+    return (vec_squashed)
+
+
+def routing(input, b_IJ, batch_size, stddev=1.0, iter_routing=1):
+    """ The routing algorithm.
+    Args:
+        input: A Tensor with [batch_size, num_caps_l=1152, 1, length(u_i)=8, 1]
+               shape, num_caps_l meaning the number of capsule in the layer l.
+    Returns:
+        A Tensor of shape [batch_size, num_caps_l_plus_1, length(v_j)=16, 1]
+        representing the vector output `v_j` in the layer l+1
+    Notes:
+        u_i represents the vector output of capsule i in the layer l, and
+        v_j the vector output of capsule j in the layer l+1.
+     """
+
+    num_caps_l = 1152
+    num_caps_l_plus_1 = 10
+    len_u_i = 8
+    len_v_j = 16
+    assert b_IJ.get_shape() == [batch_size, num_caps_l, num_caps_l_plus_1, 1, 1]
+    assert input.get_shape() == [batch_size, num_caps_l, 1, len_u_i, 1]
+    # W: [num_caps_i, num_caps_j, len_u_i, len_v_j]
+    W = tf.get_variable('Weight', shape=(1, num_caps_l, num_caps_l_plus_1, len_u_i, len_v_j), dtype=tf.float32,
+                        initializer=tf.random_normal_initializer(stddev=stddev))
+
+    # Eq.2, calc u_hat
+    # do tiling for input and W before matmul
+    # input => [batch_size, 1152, 10, 8, 1]
+    # W => [batch_size, 1152, 10, 8, 16]
+    input = tf.tile(input, [1, 1, num_caps_l_plus_1, 1, 1])
+    W = tf.tile(W, [batch_size, 1, 1, 1, 1])
+    assert input.get_shape() == [batch_size, num_caps_l, num_caps_l_plus_1, len_u_i, 1]
+
+    # in last 2 dims:
+    # [8, 16].T x [8, 1] => [16, 1] => [batch_size, 1152, 10, 16, 1]
+    # tf.scan, 3 iter, 1080ti, 128 batch size: 10min/epoch
+    # u_hat = tf.scan(lambda ac, x: tf.matmul(W, x, transpose_a=True), input, initializer=tf.zeros([1152, 10, 16, 1]))
+    # tf.tile, 3 iter, 1080ti, 128 batch size: 6min/epoch
+    u_hat = tf.matmul(W, input, transpose_a=True)
+    assert u_hat.get_shape() == [batch_size, num_caps_l, num_caps_l_plus_1, len_v_j, 1]
+
+    # In forward, u_hat_stopped = u_hat; in backward, no gradient passed back from u_hat_stopped to u_hat
+    u_hat_stopped = tf.stop_gradient(u_hat, name='stop_gradient')
+
+    # line 3,for r iterations do
+    for r_iter in range(iter_routing):
+        with tf.variable_scope('iter_' + str(r_iter)):
+            # line 4:
+            # => [batch_size, 1152, 10, 1, 1]
+            c_IJ = tf.nn.softmax(b_IJ, dim=2)
+
+            # At last iteration, use `u_hat` in order to receive gradients from the following graph
+            if r_iter == iter_routing - 1:
+                # line 5:
+                # weighting u_hat with c_IJ, element-wise in the last two dims
+                # => [batch_size, 1152, 10, 16, 1]
+                s_J = tf.multiply(c_IJ, u_hat)
+                # then sum in the second dim, resulting in [batch_size, 1, 10, 16, 1]
+                s_J = tf.reduce_sum(s_J, axis=1, keep_dims=True)
+                assert s_J.get_shape() == [batch_size, 1, num_caps_l_plus_1, len_v_j, 1]
+
+                # line 6:
+                # squash using Eq.1,
+                v_J = squash(s_J)
+                assert v_J.get_shape() == [batch_size, 1, num_caps_l_plus_1, len_v_j, 1]
+            elif r_iter < iter_routing - 1:  # Inner iterations, do not apply backpropagation
+                s_J = tf.multiply(c_IJ, u_hat_stopped)
+                s_J = tf.reduce_sum(s_J, axis=1, keep_dims=True)
+                v_J = squash(s_J)
+
+                # line 7:
+                # reshape & tile v_j from [batch_size ,1, 10, 16, 1] to [batch_size, 1152, 10, 16, 1]
+                # then matmul in the last tow dim: [16, 1].T x [16, 1] => [1, 1], reduce mean in the
+                # batch_size dim, resulting in [1, 1152, 10, 1, 1]
+                v_J_tiled = tf.tile(v_J, [1, num_caps_l, 1, 1, 1])
+                u_dot_v = tf.matmul(u_hat_stopped, v_J_tiled, transpose_a=True)
+                assert u_dot_v.get_shape() == [batch_size, num_caps_l, num_caps_l_plus_1, 1, 1]
+
+                # b_IJ += tf.reduce_sum(u_produce_v, axis=0, keep_dims=True)
+                b_IJ += u_dot_v
+
+    return v_J
+
+
+class CapsulesPolicy(object):
+    def __init__(self, sess, ob_space, ac_space, nbatch, nsteps, size_mem=256, reuse=False):  # pylint: disable=W0613
+        ob_shape = (nbatch,) + ob_space.shape
+        if ac_space.shape == ():
+            actdim = 1
+        else:
+            actdim = ac_space.shape[0]
+        X = tf.placeholder(tf.float32, ob_shape, name='Ob')  # obs
+
+        nenv = nbatch // nsteps
+        M = tf.placeholder(tf.float32, [nbatch])  # mask (done t-1)
+        S = tf.placeholder(tf.float32, [nenv, size_mem * 2])  # states
+
+        with tf.variable_scope("model", reuse=reuse):
+            # h1 = fc(X, 'pi_fc1', nh=64, init_scale=np.sqrt(2), act=tf.tanh)
+            # h2 = fc(h1, 'pi_fc2', nh=64, init_scale=np.sqrt(2), act=tf.tanh)
+
+            X_size = np.prod(ob_space.shape)
+            h2 = tf.reshape(X, shape=[nbatch, X_size])
+            h3 = fc(h2, 'h3', 2 * X_size)
+            # xs = batch_to_seq(h2, nenv, nsteps)
+            # ms = batch_to_seq(M, nenv, nsteps)
+            # h5, snew = lstm(xs, ms, S, 'lstm', nh=size_mem)
+            # h5 = seq_to_batch(h5)
+            n_capsules = 2
+            snew = S
+            b_IJ = tf.zeros([nbatch, n_capsules, n_capsules, 1, 1], dtype=np.float32)
+            h4 = tf.reshape(h3, shape=[nbatch, 2, X_size])
+            h5 = routing(input=h4, b_IJ=b_IJ, batch_size=nbatch)
+            assert h5.shape == [nbatch, 2, 1, X_size, 1]
+
+            pi = fc(h5, 'pi', actdim, act=lambda x: x, init_scale=0.01)
+            h1 = fc(X, 'vf_fc1', nh=64, init_scale=np.sqrt(2), act=tf.tanh)
+            h2 = fc(h1, 'vf_fc2', nh=64, init_scale=np.sqrt(2), act=tf.tanh)
+            vf = fc(h5, 'vf', 1, act=lambda x: x)[:, 0]
+            logstd = tf.get_variable(name="logstd", shape=[1, actdim],
+                                     initializer=tf.zeros_initializer())
+
+        pdparam = tf.concat([pi, pi * 0.0 + logstd], axis=1)
+
+        self.pdtype = make_pdtype(ac_space)
+        self.pd = self.pdtype.pdfromflat(pdparam)
+
+        # v0 = vf[0]
+        a0 = self.pd.sample()
+        neglogp0 = self.pd.neglogp(a0)
+        self.initial_state = np.zeros((nenv, size_mem * 2), dtype=np.float32)
+
+        def step(ob, state, mask):
+            return sess.run([a0, vf, snew, neglogp0], {X: ob, S: state, M: mask})
+
+        def value(ob, state, mask):
+            return sess.run(vf, {X: ob, S: state, M: mask})
+
+        # def step(ob, *_args, **_kwargs):
+        #     a, v, neglogp = sess.run([a0, vf, neglogp0], {X: ob})
+        #     return a, v, self.initial_state, neglogp
+        #
+        # def value(ob, *_args, **_kwargs):
+        #     return sess.run(vf, {X: ob})
+
+        self.X = X
+        self.M = M
+        self.S = S
+        self.pi = pi
+        self.vf = vf
+        self.step = step
+        self.value = value
 
 
 class LstmPolicy(object):
@@ -125,7 +285,6 @@ class LstmPolicy(object):
 
         def value(ob, state, mask):
             return sess.run(vf, {X: ob, S: state, M: mask})
-
 
         # def step(ob, *_args, **_kwargs):
         #     a, v, neglogp = sess.run([a0, vf, neglogp0], {X: ob})
