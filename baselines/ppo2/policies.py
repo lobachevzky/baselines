7cd7348e0328f67fcf80d2a420d0e75656f190fc
Ethan Brooks <ethanabrooks@gmail.com>
2017-12-30 12:24:24 -0500

    even more simplification of iter and preservation of some old comments

--- baselines/ppo2/policies.py
+++ baselines/ppo2/policies.py
@@ -152,24 +152,22 @@ def routing(inputs, b_IJ, output_size, stddev=1.0, iter_routing=2):
             # => [batch_size, 1152, 10, 1, 1]
             c_IJ = tf.nn.softmax(b_IJ, dim=2)
 
+            # line 5:
+            # weighting u_hat with c_IJ, element-wise in the last two dims
+            # => [batch_size, 1152, 10, 16, 1]
+            last_iter = r_iter == iter_routing - 1
             # At last iteration, use `u_hat` in order to receive gradients from the following graph
-            if r_iter == iter_routing - 1:
-                # line 5:
-                # weighting u_hat with c_IJ, element-wise in the last two dims
-                # => [batch_size, 1152, 10, 16, 1]
-                s_J = tf.multiply(c_IJ, u_hat)
-                # then sum in the second dim, resulting in [batch_size, 1, 10, 16, 1]
-                # s_J = tf.reduce_sum(s_J, axis=1, keep_dims=True)
-                # assert s_J.get_shape() == [batch_size, 1, num_caps_j, len_v_j, 1]
-                #
-                # # line 6:
-                # # squash using Eq.1,
-                # v_J = squash(s_J)
-                # assert v_J.get_shape() == [batch_size, 1, num_caps_j, len_v_j, 1]
-            elif r_iter < iter_routing - 1:  # Inner iterations, do not apply backpropagation
-                s_J = tf.multiply(c_IJ, u_hat_stopped)
+            # Inner iterations, do not apply backpropagation
+            s_J = tf.multiply(c_IJ, u_hat if last_iter else u_hat_stopped)
+
+            # then sum in the second dim, resulting in [batch_size, 1, 10, 16, 1]
             s_J = tf.reduce_sum(s_J, axis=1, keep_dims=True)
+            assert s_J.get_shape() == [batch_size, 1, num_caps_j, len_v_j, 1]
+
+            # line 6:
+            # squash using Eq.1,
             v_J = squash(s_J)
+            assert v_J.get_shape() == [batch_size, 1, num_caps_j, len_v_j, 1]
 
             # line 7:
             # reshape & tile v_j from [batch_size ,1, 10, 16, 1] to [batch_size, 1152, 10, 16, 1]
