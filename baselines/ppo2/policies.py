397cded25b21170fe8b55e3a5fe1a11101d43734
Ethan Brooks <ethanabrooks@gmail.com>
2018-01-03 13:44:27 -0500

    try reverse order of reshape

--- baselines/ppo2/policies.py
+++ baselines/ppo2/policies.py
@@ -186,8 +186,8 @@ class CapsulesPolicy(object):
 
             # Update existing hypotheses with new information.
             h3 = tiled_c + h2
-            cnew = tf.reshape(h2, [nsteps, nenv, size_lstm])
-            state_out = [.9 * c + .1 * tf.reduce_mean(cnew, axis=0), h]
+            cnew = tf.reshape(h2, [nenv, nsteps, size_lstm])
+            state_out = [.9 * c + .1 * tf.reduce_mean(cnew, axis=1), h]
             # state_out = [tf.sin(tf.reduce_sum(cnew, axis=0)), h]
             # h3, state_out = lstm(inputs=h2, c=c, h=h,
             #                      nbatch=nenv, nsteps=nsteps,
